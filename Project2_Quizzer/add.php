943b622e159fda71fd13de4aac607558e565d76b
Tadas Stasiulionis <tadas.stasiulionis@gmail.com>
2018-08-16 17:15:56 +0300

    quizzer done

--- Project2_Quizzer/add.php
+++ Project2_Quizzer/add.php
@@ -1,7 +1,63 @@
 <?php 
     include 'database.php';
 
-    if (isset($_POST))
+    if (isset($_POST['submit'])) {
+        // Get post variables
+        $question_number = $_POST['question_number'];
+        $question_text = $_POST['question_text'];
+        $correct_choice = $_POST['correct_choice'];
+        // Choices array
+        $choices = array();
+        $choices[1] = $_POST['choice1'];
+        $choices[2] = $_POST['choice2'];
+        $choices[3] = $_POST['choice3'];
+        $choices[4] = $_POST['choice4'];
+        $choices[5] = $_POST['choice5'];
+
+        // Question query
+        $query = "INSERT INTO `questions` (question_number, text)
+                    VALUES ('$question_number', '$question_text')";
+        
+        // Run query
+        $insert_row = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+        // Validate insert_row
+        if($insert_row) {
+            foreach($choices as $choice => $value) {
+                if ($value != '') {
+                    if ($correct_choice == $choice) {
+                        $is_correct = 1;
+                    } else {
+                        $is_correct = 0;
+                    }
+                    // Choice query
+                    $query = "INSERT INTO `choices` (question_number, is_correct, text)
+                                VALUES ('$question_number', '$is_correct', '$value')";
+
+                    // Run query
+                    $insert_row = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+                    // Validate insert
+                    if ($insert_row) {
+                        continue;
+                    } else {
+                        die('Error : (' . $mysqli->errno . ') ' . $mysqli->error);
+                    }
+                }
+            }
+            $msg = 'Question has been added.';
+        }
+    }
+
+    /*
+    *   Get Total Questions
+    */
+    $query = "SELECT * FROM questions";
+    
+    // Get results
+    $results = $mysqli->query($query) or die($mysqli->error. __LINE__);
+    $total = $results->num_rows;
+    $next = $total+1;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,10 +77,15 @@
     <main>
         <div class="container">
             <h2>Add A Question</h2>
+            <?php 
+                if (isset($msg)) {
+                    echo '<p>'. $msg .'</p>';
+                }
+            ?>
             <form action="add.php" method="post">
                 <p>
                     <label for="">Question Number: </label>
-                    <input type="number" name="question_number">
+                    <input type="number" name="question_number" value="<?php echo $next; ?>">
                 </p>
                 <p>
                     <label for="">Question Text: </label>
@@ -32,23 +93,23 @@
                 </p>
                 <p>
                     <label for="">Choise #1: </label>
-                    <input type="text" name="choise1">
+                    <input type="text" name="choice1">
                 </p>
                 <p>
                     <label for="">Choise #2: </label>
-                    <input type="text" name="choise2">
+                    <input type="text" name="choice2">
                 </p>
                 <p>
                     <label for="">Choise #3: </label>
-                    <input type="text" name="choise3">
+                    <input type="text" name="choice3">
                 </p>
                 <p>
                     <label for="">Choise #4: </label>
-                    <input type="text" name="choise4">
+                    <input type="text" name="choice4">
                 </p>
                 <p>
                     <label for="">Choise #5: </label>
-                    <input type="text" name="choise5">
+                    <input type="text" name="choice5">
                 </p>
                 <p>
                     <label for="">Correct Number: </label>
---
+++ quizzer.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 16, 2018 at 04:15 PM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 7.2.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quizzer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `choices`
+--
+
+CREATE TABLE `choices` (
+  `id` int(11) NOT NULL,
+  `question_number` int(11) NOT NULL,
+  `is_correct` tinyint(1) NOT NULL DEFAULT '0',
+  `text` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `choices`
+--
+
+INSERT INTO `choices` (`id`, `question_number`, `is_correct`, `text`) VALUES
+(1, 1, 1, 'PHP: Hypertext Preprocessor'),
+(2, 1, 0, 'Private Home Page'),
+(3, 1, 0, 'Personal Home Page'),
+(4, 1, 0, 'Personal Hypertext Preprocessor'),
+(5, 1, 0, 'Personal Home Pad'),
+(11, 2, 1, 'echo \"Hello World\"'),
+(12, 2, 0, '\"Hello World\"'),
+(13, 2, 0, 'Document.write(\"Hellow World);'),
+(14, 2, 0, 'var_dump(\"Hello World\");'),
+(15, 2, 0, '\'Hello World\';'),
+(16, 3, 0, 'Ruby'),
+(17, 3, 0, 'ASP'),
+(18, 3, 1, 'PHP');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `question_number` int(11) NOT NULL,
+  `text` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`question_number`, `text`) VALUES
+(1, 'What does PHP stand for?'),
+(2, 'How do you write \"Hello World\" in PHP?'),
+(3, 'What is the best server side language?');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `choices`
+--
+ALTER TABLE `choices`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`question_number`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `choices`
+--
+ALTER TABLE `choices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
